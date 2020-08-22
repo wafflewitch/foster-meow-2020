33839fb6b8b20496d6f83b0b102476b8eb4389cf
wafflewitch <jessicapruitt717@gmail.com>
2020-08-21 20:09:44 -0500

    In AnimalsAvailable component: Remove faulty cleanup function from useEffect. Comment out return statement that displays animalsStatic and un-comment return statement that displays animalsAvailable (now that we are properly fetching the data from MongoDB -- yayyy~). Change the logic in the return statement to check the length of the animalsAvailable array.

--- client/src/components/animals/AnimalsAvailable.js
+++ client/src/components/animals/AnimalsAvailable.js
@@ -15,33 +15,15 @@ const AnimalsAvailable = () => {
   useEffect(() => {
     getAnimalsAvailable();
     // eslint-disable-next-line
-    return function cleanup() {
-      clearAnimals();
-    };
   }, []);
-
-  // return (
-  //   <Fragment>
-  //     <h2>Animals Available for Fostering</h2>
-  //     <div className='container animals-container'>
-  //       {animalsAvailable !== null ? (
-  //         animalsAvailable.map((animal) => (
-  //           <AnimalObject key={animal._id} animal={animal} />
-  //         ))
-  //       ) : (
-  //         <h3>There are no animals available at this time.</h3>
-  //       )}
-  //     </div>
-  //   </Fragment>
-  // );
 
   return (
     <Fragment>
       <h2>Animals Available for Fostering</h2>
       <div className='container animals-container'>
-        {animalsStatic !== null ? (
-          animalsStatic.map((animal) => (
-            <AnimalObject key={animal.id} animal={animal} />
+        {animalsAvailable.length > 0 ? (
+          animalsAvailable.map((animal) => (
+            <AnimalObject key={animal._id} animal={animal} />
           ))
         ) : (
           <h3>There are no animals available at this time.</h3>
@@ -49,6 +31,21 @@ const AnimalsAvailable = () => {
       </div>
     </Fragment>
   );
+
+  // return (
+  //   <Fragment>
+  //     <h2>Animals Available for Fostering</h2>
+  //     <div className='container animals-container'>
+  //       {animalsStatic !== null ? (
+  //         animalsStatic.map((animal) => (
+  //           <AnimalObject key={animal.id} animal={animal} />
+  //         ))
+  //       ) : (
+  //         <h3>There are no animals available at this time.</h3>
+  //       )}
+  //     </div>
+  //   </Fragment>
+  // );
 };
 
 export default AnimalsAvailable;
